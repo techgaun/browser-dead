d8b2bc7c44b964a044992f75d3b1e6386cdada39
techgaun <coolsamar207@gmail.com>
2016-11-03 00:24:01 -0500

    some updates

--- dist/browser-dead.js
+++ dist/browser-dead.js
@@ -1,122 +1,38 @@
-/******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
+function isSupported(prop) {
+  if (prop == null) {
+    return false
+  }
+  if (prop === 'promise') {
+    return (typeof Promise !== 'undefined')
+  }
 
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
+  const elem = document.createElement('div')
+  if (prop in elem.style) {
+    return true
+  }
+  prop = prop.charAt(0).toUpperCase() + prop.slice(1)
+  const prefixes = ['Moz', 'Webkit', 'Khtml', 'Ms', 'O']
+  for (const prefix of prefixes) {
+    if (`${prefix}${prop}` in elem.style) {
+      return true
+    }
+  }
+  return false
+}
 
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
-/******/ 			return installedModules[moduleId].exports;
+export default function browserDead(opts) {
+  const defaultOpts = {
+    background: '#f00',
+    color: '#fff',
+    supportBreakpoint: 'transform',
+    browserDead: 'browser_dead'
+  }
 
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
-/******/ 		};
+  opts = Object.assign({}, defaultOpts, opts)
 
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+  // let elem = document.getElementById(opts.browserDead)
 
-/******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
-
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-
-
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ function(module, exports, __webpack_require__) {
-
-	module.exports = __webpack_require__(1);
-
-
-/***/ },
-/* 1 */
-/***/ function(module, exports) {
-
-	'use strict';
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	exports.default = browserDead;
-	function isSupported(prop) {
-	  if (prop == null) {
-	    return false;
-	  }
-	  if (prop === 'promise') {
-	    return typeof Promise !== 'undefined';
-	  }
-
-	  var elem = document.createElement('div');
-	  if (prop in elem.style) {
-	    return true;
-	  }
-	  prop = prop.charAt(0).toUpperCase() + prop.slice(1);
-	  var prefixes = ['Moz', 'Webkit', 'Khtml', 'Ms', 'O'];
-	  var _iteratorNormalCompletion = true;
-	  var _didIteratorError = false;
-	  var _iteratorError = undefined;
-
-	  try {
-	    for (var _iterator = prefixes[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-	      var prefix = _step.value;
-
-	      if ('' + prefix + prop in elem.style) {
-	        return true;
-	      }
-	    }
-	  } catch (err) {
-	    _didIteratorError = true;
-	    _iteratorError = err;
-	  } finally {
-	    try {
-	      if (!_iteratorNormalCompletion && _iterator.return) {
-	        _iterator.return();
-	      }
-	    } finally {
-	      if (_didIteratorError) {
-	        throw _iteratorError;
-	      }
-	    }
-	  }
-
-	  return false;
-	}
-
-	function browserDead(opts) {
-	  var defaultOpts = {
-	    background: '#f00',
-	    color: '#fff',
-	    supportBreakpoint: 'transform',
-	    browserDead: 'browser_dead'
-	  };
-
-	  opts = Object.assign({}, defaultOpts, opts);
-
-	  // let elem = document.getElementById(opts.browserDead)
-
-	  if (!isSupported(opts.supportBreakpoint)) {
-	    // show an overlay
-	  }
-	}
-
-/***/ }
-/******/ ]);
+  if (!isSupported(opts.supportBreakpoint)) {
+    // show an overlay
+  }
+}
---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+/* eslint no-var: off */
+
+var gulp = require('gulp')
+var cleanCSS = require('gulp-clean-css')
+var uglify = require('gulp-uglify')
+var rename = require('gulp-rename')
+var del = require('del')
+var concat = require('gulp-concat')
+var size = require('gulp-size')
+var connect = require('gulp-connect')
+var runSequence = require('run-sequence')
+
+var config = {
+  src: 'src',
+  dist: 'dist',
+}
+
+gulp.task('styles', () => {
+  gulp.src(`${config.src}/css/browser-dead.css`)
+    .pipe(concat('browser-dead.css'))
+    .pipe(gulp.dest(`${config.dist}/css`))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(cleanCSS({ compatibility: 'ie7' }))
+    .pipe(gulp.dest(`${config.dist}/css`))
+    .pipe(size())
+})
+
+gulp.task('scripts', () => {
+  gulp.src(`${config.src}/browser-dead.js`)
+    .pipe(concat('browser-dead.js'))
+    .pipe(gulp.dest(`${config.dist}/`))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(`${config.dist}/`))
+    .pipe(size())
+})
+
+gulp.task('clean', () => {
+  del.sync([
+    config.dist
+  ])
+})
+
+gulp.task('build', (done) => {
+  runSequence(
+    'clean',
+    'styles',
+    'scripts',
+    () => {
+      console.log('Build finished!!!')
+      done()
+    }
+  )
+})
+
+gulp.task('serve', ['build'], () => {
+  connect.server({
+    root: 'src',
+    port: 3000,
+    livereload: true
+  })
+  gulp.watch('src/css/browser-dead.css', ['styles'])
+  gulp.watch('src/browser-dead.js', ['scripts'])
+})
+
+// Default task
+gulp.task('default', ['serve'])
